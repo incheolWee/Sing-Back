877e9cc8dbe05aef3b366c86d1b88b0b0bf60186
Jish Shin <jisoo0375@gmail.com>
2024-07-30 10:15:17 +0900

    🎉 Spring 기본 틀 업로드

---
+++ src/main/java/com/example/sign_back/SignBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.sign_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignBackApplication.class, args);
+	}
+
+}
